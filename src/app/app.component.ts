0e431735d4b2b32795a4770dfe31173d45d13484
Kapildev Neupane <kapildevneupane55@gmail.com>
2019-06-26 18:22:28 +0545

    rotation unsuccessful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { LoadedRouterConfig } from '@angular/router/src/config';
+import { PlayerIndex } from '@angular/core/src/render3/interfaces/player';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +13,29 @@ export class AppComponent implements OnInit {
 
   public app: any;
 
-  constructor() {
-    
-
-    // this.app = new Euphony();
-    // this.app.initScene();
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
     let global: any = window;
     this.app = new global.Euphony();
-    this.app.initScene(this.canvasRef.nativeElement);
+    this.app.initMidi(() => {
+      this.app.initScene(this.canvasRef.nativeElement);      
+    });
+
+    this.http.get('/tracks/199-Tchaikovsky - Arabian Dance (Nutcracker)', {responseType: 'text'})
+      .subscribe((midiFile: any) => {
+        this.app.loadMidiFile(midiFile, () => {
+          setTimeout(() => {
+            this.app.start();
+          }, 1000);
+        })
+        console.log(midiFile);
+      })
+    // console.log(global.player);
+    // this.app.start();
+    // this.app.loadMidiFile(midiFile, function() {
+    //   global.player.play();
+    // }
   }
 }
