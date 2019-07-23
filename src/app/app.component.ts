b5ca883c6ed909e6f86a7b93414120d826692f65
Kapildev Neupane <kapildevneupane55@gmail.com>
2019-07-23 21:10:45 +0545

    merge successfully with working code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,48 +14,9 @@ export class AppComponent implements OnInit {
 
   constructor(){
 
-<<<<<<< HEAD
-  constructor(private http: HttpClient, private appService: AppService) {
-    this.appService.getMidiFiles()
-      .subscribe((midiFiles: string[]) => {
-        this.musicFiles = midiFiles;
-      });
-  }
-
-  ngOnInit() {
-    let global: any = window;
-    this.app = new global.Euphony();
-    this.app.initMidi(() => {
-      this.app.initScene(this.canvasRef.nativeElement);      
-    });
-
-    
-    // console.log(global.player);
-    // this.app.start();
-    // this.app.loadMidiFile(midiFile, function() {
-    //   global.player.play();
-    // }
-  }
-
-  midiFileChange(event: any) {
-    this.changeMidiFile(event.value);
-  }
-
-  private changeMidiFile(filename: string) {
-    this.appService.getMidiFile(filename)
-      .subscribe((midiFile: any) => {
-        console.log(midiFile);
-        this.app.loadMidiFile(midiFile, () => {
-          setTimeout(() => {
-            this.app.start();
-          }, 2000);
-        })
-      })
-=======
   }
 
   ngOnInit() {
     
->>>>>>> 38290021e5bb7bda1f916306f79fe3e7c6cdd20e
   }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Location } from '@angular/common';
 import { API_BASE_URL } from '../constants';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,7 +19,11 @@ export class DashboardComponent implements OnInit {
   public musicFiles = ['firstmidi', 'secondmidi'];
   public instruments = ['guitar', 'piano'];
 
-  constructor(private http: HttpClient) {
+  constructor(private appService: AppService, private http: HttpClient) {
+    this.appService.getMidiFiles()
+      .subscribe((midiFiles: string[]) => {
+        this.musicFiles = midiFiles;
+      });
   }
 
   ngOnInit() {
@@ -28,19 +33,27 @@ export class DashboardComponent implements OnInit {
       this.app.initScene(this.canvasRef.nativeElement);      
     });
 
-    const path = Location.joinWithSlash(API_BASE_URL, '/tracks/042-Brahms - Rhapsody in G Op-79 No-2');
-    this.http.get(path, {responseType: 'text'})
-      .subscribe((midiFile: any) => {
-        this.app.loadMidiFile(midiFile, () => {
-          setTimeout(() => {
-            this.app.start();
-          }, 2000);
-        })
-      })
+    
     // console.log(global.player);
     // this.app.start();
     // this.app.loadMidiFile(midiFile, function() {
     //   global.player.play();
     // }
+  }
+
+  midiFileChange(event: any) {
+    this.changeMidiFile(event.value);
+  }
+
+  private changeMidiFile(filename: string) {
+    this.appService.getMidiFile(filename)
+      .subscribe((midiFile: any) => {
+        console.log(midiFile);
+        this.app.loadMidiFile(midiFile, () => {
+          setTimeout(() => {
+            this.app.start();
+          }, 2000);
+        })
+      });
   }
 }
